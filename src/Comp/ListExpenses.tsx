1987d72a3bb747032e3e4aaa38372e60b6ecf610
Len Chang <lamgo.chang@gmail.com>
2022-01-19 00:41:36 +0800

    Relayout page - update the mock data

--- src/Comp/ListExpenses.tsx
+++ src/Comp/ListExpenses.tsx
@@ -9,21 +9,18 @@ import Paper from "@mui/material/Paper";
 import { Box } from "@mui/material";
 
 function createData(
-  name: string,
-  calories: number,
-  fat: number,
-  carbs: number,
-  protein: number
+  title: string,
+  category: string,
+  currency: string,
+  cost: number,
+  sharedGroup: string
 ) {
-  return { name, calories, fat, carbs, protein };
+  return { title, category, currency, cost, sharedGroup };
 }
 
 const rows = [
-  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
-  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
-  createData("Eclair", 262, 16.0, 24, 6.0),
-  createData("Cupcake", 305, 3.7, 67, 4.3),
-  createData("Gingerbread", 356, 16.0, 49, 3.9),
+  createData("Dinner", "Food", "NTD", 200, "Goup1"),
+  createData("Lunch", "Food", "NTD", 130, "Me"),
 ];
 
 export default function ListExpenses() {
@@ -33,24 +30,26 @@ export default function ListExpenses() {
         <Table sx={{ minWidth: 650 }} aria-label="simple table">
           <TableHead>
             <TableRow>
-              <TableCell>Dessert (100g serving)</TableCell>
-              <TableCell align="right">Calories</TableCell>
-              <TableCell align="right">Fat&nbsp;(g)</TableCell>
-              <TableCell align="right">Carbs&nbsp;(g)</TableCell>
+              <TableCell align="left">Title</TableCell>
+              <TableCell align="right">Category</TableCell>
+              <TableCell align="right">Cost</TableCell>
+              <TableCell align="right">Shared Group</TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
             {rows.map((row) => (
               <TableRow
-                key={row.name}
+                key={row.title}
                 sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
               >
                 <TableCell component="th" scope="row">
-                  {row.name}
+                  {row.title}
                 </TableCell>
-                <TableCell align="right">{row.calories}</TableCell>
-                <TableCell align="right">{row.fat}</TableCell>
-                <TableCell align="right">{row.carbs}</TableCell>
+                <TableCell align="right">{row.category}</TableCell>
+                <TableCell align="right">
+                  {row.currency} {row.cost}
+                </TableCell>
+                <TableCell align="right">{row.sharedGroup}</TableCell>
               </TableRow>
             ))}
           </TableBody>
