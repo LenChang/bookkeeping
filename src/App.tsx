cfbb7233e716f6432371d79a200407a11314aa86
Len Chang <lamgo.chang@gmail.com>
2022-03-15 09:50:52 +0800

    Using SpeedDial instead of Fab

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import React from "react";
-import "./App.css";
+import { Container, Fab, SpeedDialAction } from "@mui/material";
+import AddIcon from "@mui/icons-material/Add";
+import Box from "@mui/material/Box";
 import CssBaseline from "@mui/material/CssBaseline";
 import ListExpenses from "./Comp/ListExpenses";
-import { Container, Fab } from "@mui/material";
-import AddIcon from "@mui/icons-material/Add";
+import NoteAddIcon from "@mui/icons-material/NoteAdd";
+import SpeedDial from "@mui/material/SpeedDial";
+import SpeedDialIcon from "@mui/material/SpeedDialIcon";
+
+import "./App.css";
+
+const actions = [{ icon: <NoteAddIcon />, name: "New record" }];
 
 function App() {
   return (
@@ -14,13 +21,26 @@ function App() {
           <ListExpenses />
         </Container>
       </CssBaseline>
-      <Fab
+      {/* <Fab
         aria-label="Add"
         color="primary"
         style={{ position: "absolute", bottom: 50, right: 50 }}
       >
         <AddIcon />
-      </Fab>
+      </Fab> */}
+      {/* <Box sx={{ transform: "translateZ(0px)", flexGrow: 1 }}> */}
+      <SpeedDial
+        ariaLabel=""
+        sx={{ position: "absolute", bottom: 16, right: 16 }}
+        icon={<SpeedDialIcon />}
+      >
+        <SpeedDialAction
+          key={actions[0].name}
+          icon={actions[0].icon}
+          tooltipTitle={actions[0].name}
+        />
+      </SpeedDial>
+      {/* </Box> */}
     </>
   );
 }
