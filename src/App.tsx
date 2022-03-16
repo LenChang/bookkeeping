67288ed28bbdb9d0b2cbcef565ae7c83d7b790dd
Len Chang <lamgo.chang@gmail.com>
2022-03-16 09:31:02 +0800

    Delete unused code and layout title date

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,5 @@
 import React from "react";
-import { Container, Fab, SpeedDialAction } from "@mui/material";
-import AddIcon from "@mui/icons-material/Add";
-import Box from "@mui/material/Box";
-import CssBaseline from "@mui/material/CssBaseline";
+import { Box, Container, SpeedDialAction, CssBaseline } from "@mui/material";
 import ListExpenses from "./Comp/ListExpenses";
 import NoteAddIcon from "@mui/icons-material/NoteAdd";
 import SpeedDial from "@mui/material/SpeedDial";
@@ -18,17 +15,14 @@ function App() {
       <CssBaseline>
         <CssBaseline />
         <Container fixed>
+          <Box margin={2} sx={{ display: "flex", justifyContent: "center" }}>
+            16th, Mar 2022 09:29
+          </Box>
+        </Container>
+        <Container fixed>
           <ListExpenses />
         </Container>
       </CssBaseline>
-      {/* <Fab
-        aria-label="Add"
-        color="primary"
-        style={{ position: "absolute", bottom: 50, right: 50 }}
-      >
-        <AddIcon />
-      </Fab> */}
-      {/* <Box sx={{ transform: "translateZ(0px)", flexGrow: 1 }}> */}
       <SpeedDial
         ariaLabel=""
         sx={{ position: "absolute", bottom: 16, right: 16 }}
@@ -40,7 +34,6 @@ function App() {
           tooltipTitle={actions[0].name}
         />
       </SpeedDial>
-      {/* </Box> */}
     </>
   );
 }
